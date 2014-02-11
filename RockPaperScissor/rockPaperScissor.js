b79325e38761d65b76c4f5d8dd1a381ef0e5a081
Michelle Wegenstein <mwegenstein@gmail.com>
2014-02-11 11:36:00 -0800

    break down steps for rock paper scissor exercise
    adapting from code academy

---
+++ RockPaperScissor/rockPaperScissor.js
@@ -0,0 +1,6 @@
+rockpaperscissor.js
+// Create a game between the computer and the user playing rock paper scissors. Breaking down the problem:
+// 1. Create a prompt for the user choice
+// 2. using mathrandom set numbers equal to rock/paper/scissor
+// 3. Create function for return value of each rock/paper/scissor case
+// 4. Set that inside of a compare function to determine who wins. 
